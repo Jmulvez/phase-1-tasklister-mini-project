6a3abdbdb5cb4e978e93adf905ee9c4c7261c6c5
John <johnmulvey48@yahoo.com>
2021-12-30 11:11:45 -0500

    git@github.com:Jmulvez/phase-1-tasklister-mini-project.git

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function() {
+  console.log();
+}
+
+document.querySelector('form').addEventListener('click', function(event)) {
+  document.getElementByID('post').event.preventDefault();
+}, false);
+
+document.querySelector('create-task-form').addEventListener
+('click', () => console.log(Success!))
+
+function handDelete(t){
+  t.target.parentNode.remove()
+}
+
+function addEventListenerToDeleteButtons(){
+  let buttons = document.getElementsByClassName
+  ('delete_btn')
+  for(let btn of buttons){
+    btn.addEventListener('click', handDelete)
+  }
+}
+addEventListenerToDeleteButtons()
